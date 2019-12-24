7610a9459d0f848bd06569c3d171778d30d0a705
Nick Sagona <nick@nolainteractive.com>
2019-12-24 10:49:57 -0600

    Work on string converter class

--- src/StringConverter.php
+++ src/StringConverter.php
@@ -27,6 +27,14 @@ class StringConverter
 {
 
     /**
+     * Allowed keywords for converting cases
+     * @var array
+     */
+    protected static $allowedCases = [
+        'titlecase', 'camelcase', 'kebabcase', 'dash', 'snakecase', 'underscore', 'namespace', 'path', 'url', 'uri'
+    ];
+
+    /**
      * Convert a string from one case to another
      *
      * @param string $name
@@ -35,49 +43,85 @@ class StringConverter
      */
     public static function __callStatic($name, $arguments)
     {
-        [$from, $to] = explode('to', strtolower($name));
-        $string      = $arguments[0] ?? null;
-        $separator   = $arguments[1] ?? null;
-        $result      = null;
+        [$from, $to]   = explode('to', strtolower($name));
+        $string        = $arguments[0] ?? null;
+        $preserveCase  = (array_key_exists(1, $arguments) && is_bool($arguments[1])) ? $arguments[1] : null;
+        $separator     = null;
+        $prevSeparator = null;
+        $result        = null;
 
-        switch ($from) {
+        switch ($to) {
             case 'titlecase':
             case 'camelcase':
-                switch ($to) {
-                    case 'kebabcase':
-                    case 'dash':
-                        $separator = '-';
-                        break;
-                    case 'snakecase':
-                    case 'underscore':
-                        $separator = '_';
-                        break;
-                    case 'namespace':
-                        $separator = '\\';
-                        break;
-                    case 'path':
-                        $separator = '/';
-                        break;
-                }
-                $result = strtolower(self::convertFromCamelCase($string, $separator));
+                $preserveCase = true;
                 break;
             case 'kebabcase':
             case 'dash':
                 $separator = '-';
+                if (null === $preserveCase) {
+                    $preserveCase = false;
+                }
                 break;
             case 'snakecase':
             case 'underscore':
                 $separator = '_';
+                if (null === $preserveCase) {
+                    $preserveCase = false;
+                }
                 break;
             case 'namespace':
                 $separator = '\\';
+                if (null === $preserveCase) {
+                    $preserveCase = true;
+                }
                 break;
             case 'path':
+                $separator = DIRECTORY_SEPARATOR;
+                if (null === $preserveCase) {
+                    $preserveCase = true;
+                }
+                break;
+            case 'uri':
+            case 'url':
                 $separator = '/';
+                if (null === $preserveCase) {
+                    $preserveCase = true;
+                }
                 break;
         }
 
-        if ((null === $result) && (null !== $separator)) {
+        switch ($from) {
+            case 'titlecase':
+            case 'camelcase':
+                $result = self::convertFromCamelCase($string, $separator, $preserveCase);
+                if ($to == 'titlecase') {
+                    $result = ucfirst($result);
+                }
+                if ($to == 'camelcase') {
+                    $result = lcfirst($result);
+                }
+                break;
+            case 'kebabcase':
+            case 'dash':
+                $prevSeparator = '-';
+                break;
+            case 'snakecase':
+            case 'underscore':
+                $prevSeparator = '_';
+                break;
+            case 'namespace':
+                $prevSeparator = '\\';
+                break;
+            case 'path':
+                $prevSeparator = DIRECTORY_SEPARATOR;
+                break;
+            case 'url':
+            case 'uri':
+                $prevSeparator = '/';
+                break;
+        }
+
+        if (null === $result) {
             switch ($from) {
                 case 'titlecase':
                     $result = ucfirst(self::convertToCamelCase($string, $separator));
@@ -85,6 +129,25 @@ class StringConverter
                 case 'camelcase':
                     $result = self::convertToCamelCase($string, $separator);
                     break;
+            }
+
+            if (null === $result) {
+                switch ($to) {
+                    case 'titlecase':
+                        $result = ucfirst(self::convertToCamelCase($string, $prevSeparator));
+                        break;
+                    case 'camelcase':
+                        $result = lcfirst(self::convertToCamelCase($string, $prevSeparator));
+                        break;
+                    default:
+                        if ($preserveCase) {
+                            $string = implode($prevSeparator, array_map('ucfirst', explode($prevSeparator, $string)));
+                        }
+                        $result = str_replace($prevSeparator, $separator, $string);
+                        if ($preserveCase === false) {
+                            $result = strtolower($result);
+                        }
+                }
             }
         }
 
@@ -94,21 +157,22 @@ class StringConverter
     /**
      * Convert a camelCase string using the $separator value passed
      *
-     * @param string $string
-     * @param string $separator
+     * @param string  $string
+     * @param string  $separator
+     * @param boolean $preserveCase
      * @return string
      */
-    public static function convertFromCamelCase($string, $separator)
+    public static function convertFromCamelCase($string, $separator, $preserveCase = false)
     {
         $stringAry = str_split($string);
         $converted = null;
 
         foreach ($stringAry as $i => $char) {
             $converted .= ($i == 0) ?
-                strtolower($char) : ((ctype_upper($char)) ? ($separator . strtolower($char)) : $char);
+                $char : ((ctype_upper($char)) ? ($separator . $char) : $char);
         }
 
-        return $converted;
+        return ($preserveCase) ? $converted : strtolower($converted);
     }
 
     /**
