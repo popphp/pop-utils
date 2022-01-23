dd8a69cbf83cb69ec672890cc10c77f9a1bbb5d7
Nick Sagona <nick@nolainteractive.com>
2022-01-22 19:16:20 -0600

    Update version

--- src/Str.php
+++ src/Str.php
@@ -21,7 +21,7 @@ namespace Pop\Utils;
  * @author     Nick Sagona, III <dev@nolainteractive.com>
  * @copyright  Copyright (c) 2009-2021 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
- * @version    1.2.0
+ * @version    1.3.0
  */
 class Str
 {
